d1ca5901a283505d282eb00bc41c9d76b22d4889
Tryblog <50934786+TRYblog@users.noreply.github.com>
2021-02-22 18:47:36 +0800

    Update client.go

--- src/net/http/client.go
+++ src/net/http/client.go
@@ -16,6 +16,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"reflect"
@@ -215,7 +216,7 @@ func send(ireq *Request, rt RoundTripper, deadline time.Time) (resp *Response, d
 
 	if req.RequestURI != "" {
 		req.closeBody()
-		return nil, alwaysFalse, errors.New("http: Request.RequestURI can't be set in client requests")
+		return nil, alwaysFalse, errors.New("http: Request.RequestURI can't be set in client requests.")
 	}
 
 	// forkReq forks req into a shallow clone of ireq the first
@@ -264,25 +265,6 @@ func send(ireq *Request, rt RoundTripper, deadline time.Time) (resp *Response, d
 		}
 		return nil, didTimeout, err
 	}
-	if resp == nil {
-		return nil, didTimeout, fmt.Errorf("http: RoundTripper implementation (%T) returned a nil *Response with a nil error", rt)
-	}
-	if resp.Body == nil {
-		// The documentation on the Body field says “The http Client and Transport
-		// guarantee that Body is always non-nil, even on responses without a body
-		// or responses with a zero-length body.” Unfortunately, we didn't document
-		// that same constraint for arbitrary RoundTripper implementations, and
-		// RoundTripper implementations in the wild (mostly in tests) assume that
-		// they can use a nil Body to mean an empty one (similar to Request.Body).
-		// (See https://golang.org/issue/38095.)
-		//
-		// If the ContentLength allows the Body to be empty, fill in an empty one
-		// here to ensure that it is non-nil.
-		if resp.ContentLength > 0 && req.Method != "HEAD" {
-			return nil, didTimeout, fmt.Errorf("http: RoundTripper implementation (%T) returned a *Response with content length %d but a nil Body", rt, resp.ContentLength)
-		}
-		resp.Body = io.NopCloser(strings.NewReader(""))
-	}
 	if !deadline.IsZero() {
 		resp.Body = &cancelTimerBody{
 			stop:          stopTimer,
@@ -320,7 +302,7 @@ func knownRoundTripperImpl(rt RoundTripper, req *Request) bool {
 		return true
 	}
 	// There's a very minor chance of a false positive with this.
-	// Instead of detecting our golang.org/x/net/http2.Transport,
+	// Insted of detecting our golang.org/x/net/http2.Transport,
 	// it might detect a Transport type in a different http2
 	// package. But I know of none, and the only problem would be
 	// some temporarily leaked goroutines if the transport didn't
@@ -696,7 +678,7 @@ func (c *Client) do(req *Request) (retres *Response, reterr error) {
 			// fails, the Transport won't reuse it anyway.
 			const maxBodySlurpSize = 2 << 10
 			if resp.ContentLength == -1 || resp.ContentLength <= maxBodySlurpSize {
-				io.CopyN(io.Discard, resp.Body, maxBodySlurpSize)
+				io.CopyN(ioutil.Discard, resp.Body, maxBodySlurpSize)
 			}
 			resp.Body.Close()
 
@@ -821,8 +803,8 @@ func defaultCheckRedirect(req *Request, via []*Request) error {
 //
 // See the Client.Do method documentation for details on how redirects
 // are handled.
-func Post(url, contentType string, body io.Reader) (resp *Response, err error) {
-	return DefaultClient.Post(url, contentType, body)
+func Post(url, contentType string,cook string, body io.Reader) (resp *Response, err error) {
+	return DefaultClient.Post(url, contentType,cook, body)
 }
 
 // Post issues a POST to the specified URL.
@@ -836,12 +818,13 @@ func Post(url, contentType string, body io.Reader) (resp *Response, err error) {
 //
 // See the Client.Do method documentation for details on how redirects
 // are handled.
-func (c *Client) Post(url, contentType string, body io.Reader) (resp *Response, err error) {
+func (c *Client) Post(url, contentType string,cook string, body io.Reader) (resp *Response, err error) {
 	req, err := NewRequest("POST", url, body)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Content-Type", contentType)
+	req.Header.Set("Cookie", cook)
 	return c.Do(req)
 }
 
@@ -858,8 +841,8 @@ func (c *Client) Post(url, contentType string, body io.Reader) (resp *Response, 
 //
 // See the Client.Do method documentation for details on how redirects
 // are handled.
-func PostForm(url string, data url.Values) (resp *Response, err error) {
-	return DefaultClient.PostForm(url, data)
+func PostForm(url string,cook string, data url.Values) (resp *Response, err error) {
+	return DefaultClient.PostForm(url,cook, data)
 }
 
 // PostForm issues a POST to the specified URL,
@@ -873,8 +856,8 @@ func PostForm(url string, data url.Values) (resp *Response, err error) {
 //
 // See the Client.Do method documentation for details on how redirects
 // are handled.
-func (c *Client) PostForm(url string, data url.Values) (resp *Response, err error) {
-	return c.Post(url, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+func (c *Client) PostForm(url string,cook string, data url.Values) (resp *Response, err error) {
+	return c.Post(url, "application/x-www-form-urlencoded", cook,strings.NewReader(data.Encode()))
 }
 
 // Head issues a HEAD to the specified URL. If the response is one of
